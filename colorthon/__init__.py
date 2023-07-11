17a6ad55fd0f5b286ea6b99ffae581549d4fad66
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2023-07-11 22:51:40 +0300

    Update __init__.py

--- colorthon/__init__.py
+++ colorthon/__init__.py
@@ -1,25 +1,13 @@
 # programmer Mmdrza.Com
-from .Text import Color
+from .utils import Colors, Back
+# * -------------------------- * #
 
-
-class console():
-    def __init__(self):
-        self.red = Color.red
-        self.green = Color.green
-        self.yellow = Color.yellow
-        self.cyan = Color.cyan
-        self.blue = Color.blue
-        self.white = Color.white
-        self.grey = Color.grey
-        self.magenta = Color.magenta
-        self.reset = Color.reset
-
-
-__version__: str = "v4.6.3"
+__version__: str = "v2.6.3"
 __license__: str = "MIT"
 __author__: str = "Mohammadreza Fekri"
 __email__: str = "PyMmdrza@gmail.com"
-__description__: str = "Fast And Easy Generated Wallet on Python With Blockthon"
+__description__: str = "Colorthon For Color Text In Python Console , Terminal , CMD PRINT"
+
 
 __all__: list = [
     "__version__",
@@ -27,6 +15,5 @@ __all__: list = [
     "__author__",
     "__email__",
     "__description__",
-    "utils",
-    "Text"
+    "utils"
 ]
