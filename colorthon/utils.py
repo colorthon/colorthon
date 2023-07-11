4a5ba7eac758835f88552a77bb756e07b4f569d5
MMDRZA <95309780+Pymmdrza@users.noreply.github.com>
2023-07-11 22:51:14 +0300

    Update utils.py

--- colorthon/utils.py
+++ colorthon/utils.py
@@ -1,13 +1,21 @@
-import sys, os
+class Colors:
+    BLACK = '\33[30m'
+    RED = '\33[31m'
+    GREEN = '\33[32m'
+    YELLOW = '\33[33m'
+    BLUE = '\33[34m'
+    MAGENTA = '\33[35m'
+    CYAN = '\33[36m'
+    WHITE = '\33[37m'
+    GREY = '\33[2m'
+    RESET = '\033[0m'
 
-
-class Colors():
-    Red = '\33[31m'
-    Green = '\33[32m'
-    Yellow = '\33[33m'
-    Blue = '\33[34m'
-    Magenta = '\33[35m'
-    Cyan = '\33[36m'
-    White = '\33[37m'
-    Grey = '\33[2m'
-    Reset = '\033[0m'
+class Back:
+    BLACK = "\33[40m"
+    RED = "\33[41m"
+    GREEN = "\33[42m"
+    YELLOW = "\33[43m"
+    BLUE = "\33[44m"
+    MAGENTA = "\33[45m"
+    CYAN = "\33[46m"
+    WHITE = "\33[47m"
